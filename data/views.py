a33b18451cf6af522c402cd6300cf27fe776b40c
srujanoza <sonuoza24@gmail.com>
2023-10-31 14:20:19 -0400

    Initial commit for data updation

--- data/views.py
+++ data/views.py
@@ -35,24 +35,42 @@ def get_file_names(request, user):
 @csrf_exempt
 @authenticate_user
 def upload_file(request, user):
-    file_related_data = json.loads(request.POST.get('data'))
-    file_title = file_related_data.get('file_name')
-    file_type = file_related_data.get('file_type')
-    uploaded_file = request.FILES.get('uploaded_file')
-    if file_type == "csv":
-        processed_data = process_file(uploaded_file)
-    elif file_type == "xlsx":
-        processed_data = process_xlsx_file(uploaded_file)
+    if 'update_value' in request.POST:
+        # Handle the request to update a specific value in the file data
+        file_data_id = request.POST.get('file_data_id')
+        row_number = int(request.POST.get('row_number'))
+        column_name = request.POST.get('column_name')
+        new_value = request.POST.get('new_value')
+
+        try:
+            file_data = FileData.objects.get(pk=file_data_id)
+        except FileData.DoesNotExist:
+            return JsonResponse({"data": "", "error": "File data not found."}, status=404)
+
+        # Update the specific value in the data
+        file_data.data[str(row_number)][column_name] = new_value
+        file_data.save()
+
+        return JsonResponse({"data": {"message": "File data updated successfully."}, "error": ""}, status=200)
     else:
-        return JsonResponse({"data": "", "error": "File type is not supported."}, status=415)
+        file_related_data = json.loads(request.POST.get('data'))
+        file_title = file_related_data.get('file_name')
+        file_type = file_related_data.get('file_type')
+        uploaded_file = request.FILES.get('uploaded_file')
+        if file_type == "csv":
+            processed_data = process_file(uploaded_file)
+        elif file_type == "xlsx":
+            processed_data = process_xlsx_file(uploaded_file)
+        else:
+            return JsonResponse({"data": "", "error": "File type is not supported."}, status=415)
 
-    category_obj = Category.objects.get(pk=file_related_data.get('file_category'))
+        category_obj = Category.objects.get(pk=file_related_data.get('file_category'))
 
-    file_data_object = FileData(title=file_title, category=category_obj, data=processed_data, uploaded_by=user)
-    file_data_object.save()
+        file_data_object = FileData(title=file_title, category=category_obj, data=processed_data, uploaded_by=user)
+        file_data_object.save()
 
-    message = "File processed successfully."
-    return JsonResponse({"data": {"message": message}, "error": ""}, status=200)
+        message = "File processed successfully."
+        return JsonResponse({"data": {"message": message}, "error": ""}, status=200)
 
 
 def process_xlsx_file(uploaded_file):
@@ -99,3 +117,17 @@ def get_file_data(request, user):
     serializer = FileDataSerializer(file_data, many=False)
     data = serializer.data
     return JsonResponse({"data": {"data": data}, "error": ""}, status=200)
+
+@csrf_exempt
+def update_records(request):
+    updated_file_data = json.loads(request.POST.get('data'))
+    file_title = file_related_data.get('file_name')
+    file_type = file_related_data.get('file_type')
+    updated_file = request.FILES.get('uploaded_file')
+    if file_type == "csv":
+        processed_data = process_file(uploaded_file)
+    elif file_type == "xlsx":
+        processed_data = process_xlsx_file(uploaded_file)
+    else:
+        return JsonResponse({"data": "", "error": "File type is not supported."}, status=415)
+
