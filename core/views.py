4e7b3aba8b0719b9927ae98a126d8c0bd2d8e933
kinjal <kinjaldoshi1398@gmail.com>
2023-11-03 18:15:03 -0400

    Error handling code updated

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.db import DatabaseError
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from rest_framework.parsers import JSONParser
 
 from core.decorators import authenticate_user
@@ -34,72 +35,66 @@ def user_list(request, user):
         return JsonResponse({"data": "", "error": "User does not exist."}, status=404)
 
     except Exception as e:
-        return JsonResponse({"data": "", "error": f"Internal server error: {str(e)}"}, status=500)
+        return JsonResponse({"data": "", "error": str(e)}, status=500)
 
 
 @csrf_exempt
+@require_POST
 @authenticate_user
 def change_password(request, user):
-    error = ""
     message = ""
-    if request.method == "POST":
-        try:
-            data = JSONParser().parse(request)
-            new_password = data.get('password')
+    try:
+        data = JSONParser().parse(request)
+        new_password = data.get('password')
 
-            if new_password:
-                user.set_password(new_password)
+        if new_password:
+            user.set_password(new_password)
 
-                if data.get('fromForgetPassword'):
-                    user.is_password_reset = True
-                user.save()
+            if data.get('fromForgetPassword'):
+                user.is_password_reset = True
+            user.save()
 
-                message = "Password changed successfully."
-                error = ""
-            else:
-                error = "New password not provided."
-        except ObjectDoesNotExist:
-            return JsonResponse({"data": "", "error": "User does not exist."}, status=404)
-        except Exception as e:
-            return JsonResponse({"data": "", "error": f"Internal server error: {str(e)}"}, status=500)
-    else:
-        error = "Invalid request method. Only POST is supported."
+            message = "Password changed successfully."
+            error = ""
+        else:
+            error = "New password not provided."
+    except ObjectDoesNotExist:
+        return JsonResponse({"data": "", "error": "User does not exist."}, status=404)
+    except Exception as e:
+        return JsonResponse({"data": "", "error": str(e)}, status=500)
 
     return JsonResponse({"data": {"message": message}, "error": error}, status=200)
 
 
 @csrf_exempt
+@require_POST
 def forget_password(request):
     try:
-        if request.method == "POST":
-            data = JSONParser().parse(request)
-            username_or_email = data.get('usernameOrEmail')
-            user_query_data = UserModel.objects.filter(Q(username=username_or_email) | Q(email=username_or_email))
-            if user_query_data.exists():
-                user_instance = user_query_data[0]
-                code = user_instance.generate_forget_password_key()
-                user_instance.is_password_reset = False
-                user_instance.save()
-                # TODO: Send Email Function Call Goes Here
-                message = "An email has been sent to you. Please enter the code to change your password."
-                data = {"message": message,
-                        "username": user_instance.username,
-                        "code": code}
-                return JsonResponse({"data": data, "error": ""}, status=200)
-            else:
-                return JsonResponse({"data": "", "error": "User not found. Please check your username or email."},
-                                    status=404)
+        data = JSONParser().parse(request)
+        username_or_email = data.get('usernameOrEmail')
+        user_query_data = UserModel.objects.filter(Q(username=username_or_email) | Q(email=username_or_email))
+        if user_query_data.exists():
+            user_instance = user_query_data[0]
+            code = user_instance.generate_forget_password_key()
+            user_instance.is_password_reset = False
+            user_instance.save()
+            # TODO: Send Email Function Call Goes Here
+            message = "An email has been sent to you. Please enter the code to change your password."
+            data = {"message": message,
+                    "username": user_instance.username,
+                    "code": code}
+            return JsonResponse({"data": data, "error": ""}, status=200)
         else:
-            return JsonResponse({"data": "", "error": "Invalid request method. Only POST is supported."},
-                                status=405)
+            return JsonResponse({"data": "", "error": "User not found. Please check your username or email."},
+                                status=404)
     except Exception as e:
-        return JsonResponse({"data": "", "error": f"Internal server error: {str(e)}"}, status=500)
+        return JsonResponse({"data": "", "error": str(e)}, status=500)
 
 
 @csrf_exempt
+@require_POST
 def create_user(request):
-    if request.method == "POST":
-        data = JSONParser().parse(request)
-        print(data)
+    data = JSONParser().parse(request)
+    print(data)
 
     return JsonResponse({"error": "Invalid JSON data."}, status=200)
--- data/views.py
+++ data/views.py
@@ -7,7 +7,7 @@ import json
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 
 from core.decorators import authenticate_user
 from data.models import FileData, Category
@@ -16,16 +16,14 @@ from data.serializers import CategorySerializer, FileDataSerializer, FileDataIDS
 
 # Create your views here.
 @csrf_exempt
+@require_GET
 @authenticate_user
 def get_file_categories(request, user):
     try:
-        if request.method == "GET":
-            categories = Category.objects.all()
-            serializer = CategorySerializer(categories, many=True)
-            category_data = serializer.data
-            return JsonResponse({"data": {"data": category_data}, "error": ""}, status=200)
-        else:
-            return JsonResponse({"data": "", "error": "Invalid request method. Only GET is supported."}, status=405)
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        category_data = serializer.data
+        return JsonResponse({"data": {"data": category_data}, "error": ""}, status=200)
     except ObjectDoesNotExist:
         return JsonResponse({"data": "", "error": "No file categories found."}, status=404)
     except Exception as e:
@@ -37,13 +35,10 @@ def get_file_categories(request, user):
 @authenticate_user
 def get_file_names(request, user):
     try:
-        if request.method == "GET":
-            file_data = FileData.objects.all()
-            serializer = FileDataIDSerializer(file_data, many=True)
-            data = serializer.data
-            return JsonResponse({"data": {"data": data}, "error": ""}, status=200)
-        else:
-            return JsonResponse({"data": "", "error": "Invalid request method. Only GET is supported."}, status=405)
+        file_data = FileData.objects.all()
+        serializer = FileDataIDSerializer(file_data, many=True)
+        data = serializer.data
+        return JsonResponse({"data": {"data": data}, "error": ""}, status=200)
     except ObjectDoesNotExist:
         return JsonResponse({"data": "", "error": "No file names found."}, status=404)
     except Exception as e:
@@ -51,38 +46,35 @@ def get_file_names(request, user):
 
 
 @csrf_exempt
+@require_POST
 @authenticate_user
 def upload_file(request, user):
     try:
-        if request.method == "POST":
-            file_related_data = json.loads(request.POST.get('data'))
-            file_title = file_related_data.get('file_name')
-            file_type = file_related_data.get('file_type')
-            uploaded_file = request.FILES.get('uploaded_file')
+        file_related_data = json.loads(request.POST.get('data'))
+        file_title = file_related_data.get('file_name')
+        file_type = file_related_data.get('file_type')
+        uploaded_file = request.FILES.get('uploaded_file')
 
-            if file_type == "csv":
-                processed_data = process_file(uploaded_file)
-            elif file_type == "xlsx":
-                processed_data = process_xlsx_file(uploaded_file)
-            else:
-                return JsonResponse({"data": "", "error": "File type is not supported."}, status=415)
-
-            category_id = file_related_data.get('file_category')
-            try:
-                category_obj = Category.objects.get(pk=category_id)
-            except ObjectDoesNotExist:
-                return JsonResponse({"data": "", "error": f"Category with ID {category_id} not found."}, status=404)
-
-            file_data_object = FileData(title=file_title, category=category_obj, data=processed_data, uploaded_by=user)
-            file_data_object.save()
-
-            message = "File processed successfully."
-            return JsonResponse({"data": {"message": message}, "error": ""}, status=200)
+        if file_type == "csv":
+            processed_data = process_file(uploaded_file)
+        elif file_type == "xlsx":
+            processed_data = process_xlsx_file(uploaded_file)
         else:
-            return JsonResponse({"data": "", "error": "Invalid request method. Only POST is supported."}, status=405)
+            return JsonResponse({"data": "", "error": "File type is not supported."}, status=415)
 
+        category_id = file_related_data.get('file_category')
+        try:
+            category_obj = Category.objects.get(pk=category_id)
+        except ObjectDoesNotExist:
+            return JsonResponse({"data": "", "error": f"Category with ID {category_id} not found."}, status=404)
+
+        file_data_object = FileData(title=file_title, category=category_obj, data=processed_data, uploaded_by=user)
+        file_data_object.save()
+
+        message = "File processed successfully."
+        return JsonResponse({"data": {"message": message}, "error": ""}, status=200)
     except Exception as e:
-        return JsonResponse({"data": "", "error": f"Internal server error: {str(e)}"}, status=500)
+        return JsonResponse({"data": "", "error": {str(e)}}, status=500)
 
 
 def process_xlsx_file(uploaded_file):
@@ -104,9 +96,9 @@ def process_xlsx_file(uploaded_file):
             count += 1
         return file_data_json
     except InvalidFileException:
-        raise ValueError("Invalid file format. This is not a valid Excel file.")
+        return JsonResponse({"data": "", "error": "Invalid file format. This is not a valid Excel file."}, status=500)
     except Exception as e:
-        raise ValueError(f"Error processing the Excel file: {str(e)}")
+        return JsonResponse({"data": "", "error": f"Error processing the Excel file: {str(e)}"}, status=500)
 
 
 def process_file(uploaded_file):
@@ -121,11 +113,12 @@ def process_file(uploaded_file):
             count += 1
         return json_data
     except UnicodeDecodeError:
-        raise ValueError("Invalid file encoding. The file should be in UTF-8 encoding.")
+        error = "Invalid file encoding. The file should be in UTF-8 encoding."
     except csv.Error as e:
-        raise ValueError(f"CSV parsing error: {str(e)}")
+        error = f"CSV parsing error: {str(e)}"
     except Exception as e:
-        raise ValueError(f"Error processing the CSV file: {str(e)}")
+        error = f"Error processing the CSV file: {str(e)}"
+    return JsonResponse({"data": "", "error": error}, status=500)
 
 
 @csrf_exempt
@@ -133,20 +126,17 @@ def process_file(uploaded_file):
 @authenticate_user
 def get_file_data(request, user):
     try:
-        if request.method == "GET":
-            file_id = request.GET.get('id', None)
+        file_id = request.GET.get('id', None)
 
-            if file_id is None:
-                return JsonResponse({"data": "", "error": "Missing 'id' parameter in the request."}, status=400)
+        if file_id is None:
+            return JsonResponse({"data": "", "error": "Missing 'id' parameter in the request."}, status=400)
 
-            try:
-                file_data = FileData.objects.get(pk=file_id)
-                serializer = FileDataSerializer(file_data, many=False)
-                data = serializer.data
-                return JsonResponse({"data": {"data": data}, "error": ""}, status=200)
-            except ObjectDoesNotExist:
-                return JsonResponse({"data": "", "error": f"File with ID {file_id} not found."}, status=404)
-        else:
-            return JsonResponse({"data": "", "error": "Invalid request method. Only GET is supported."}, status=405)
+        try:
+            file_data = FileData.objects.get(pk=file_id)
+            serializer = FileDataSerializer(file_data, many=False)
+            data = serializer.data
+            return JsonResponse({"data": {"data": data}, "error": ""}, status=200)
+        except ObjectDoesNotExist:
+            return JsonResponse({"data": "", "error": f"File with ID {file_id} not found."}, status=404)
     except Exception as e:
-        return JsonResponse({"data": "", "error": f"Internal server error: {str(e)}"}, status=500)
+        return JsonResponse({"data": "", "error": str(e)}, status=500)
