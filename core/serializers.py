19abae567235f596902d19227fe950d7f86644c9
jhanviVadher29 <jhanvivadher10@gmail.com>
2023-11-05 02:07:09 -0500

    single login logout

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,42 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import Token
+
+from core.models import CustomUserRoles
+from django.utils.crypto import get_random_string
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, AuthUser
 
 UserModel = get_user_model()
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        user = self.user
+
+        if user.is_deleted:
+            raise serializers.ValidationError(
+                "Your account has been deactivated. Please contact admin if this was a mistake.")
+
+        return data
+
+    def get_token(cls, user: AuthUser) -> Token:
+        token = super().get_token(user)
+        user.user_verification_key = get_random_string(length=12)
+        user.save()
+        token['user_verification_key'] = user.user_verification_key  # Include the verification key
+
+        return token
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
-        fields = ['username', 'email', 'is_staff']
+        fields = ['id', 'username', 'email', 'is_staff']
+
+
+class CustomUserRoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUserRoles
+        exclude = ['user']
