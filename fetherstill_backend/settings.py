a235f59b5325b5c2a1af275eabccd6e744100d58
dataSHETHinks <sarahansari2930@gmail.com>
2023-11-14 00:36:48 -0500

    Email-Microservices-Implemented

--- fetherstill_backend/settings.py
+++ fetherstill_backend/settings.py
@@ -206,3 +206,10 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+SENDGRID_API_KEY = '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'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
