00f4e0a9fbe459cdc76c991672d513aaacdf5d88
Chintal Raval <chintaldraval@gmail.com>
2023-11-30 21:10:30 -0500

    Fixed time zone

--- fetherstill_backend/settings.py
+++ fetherstill_backend/settings.py
@@ -149,11 +149,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Toronto'
+TIME_ZONE = 'America/New_York'
 
-USE_I18N = True
-
-USE_TZ = True
+# USE_I18N = True
+#
+# USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
